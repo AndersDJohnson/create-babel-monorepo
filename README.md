5d61ab2d9def781b8587efae1301f5b3bf932524
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2020-07-11 17:04:20 -0500

    chore: init

---
+++ README.md
@@ -0,0 +1,14 @@
+# create-babel-monorepo
+
+Like `create-react-app`, but for a basic Babel setup.
+
+Create an app in a new `./my-app` directory:
+
+```shell
+$ npm init babel my-app
+```
+[<kbd>copy</kbd>](https://copyhaste.com/c/?t=npm%20init%20babel%20my-app)
+
+* :sparkles: Auto-installs dependencies.
+* :fast_forward: Out-of-the-box [preset-env](https://babeljs.io/docs/en/babel-preset-env) for modern syntax & [transform-runtime](https://babeljs.io/docs/en/babel-plugin-transform-runtime) for `async`/`await`.
+* 🛠️ Baked-in `build` and `build:watch` scripts, `.gitignore` & more!
---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const createMonorepo = require("base-create-monorepo");
+
+createMonorepo("babel-monorepo", {
+  package: {
+    keywords: ["create-babel-monorepo"],
+    scripts: {
+      start: "lerna run start",
+      build: "lerna run build",
+      "build:watch": "lerna run --parallel build:watch",
+    },
+  },
+  eachPackage: {
+    dependencies: ["@babel/runtime"],
+    devDependencies: [
+      "@babel/core",
+      "@babel/cli",
+      "@babel/node",
+      "@babel/preset-env",
+      "@babel/plugin-transform-runtime",
+    ],
+    package: {
+      main: "dist/index.js",
+      keywords: ["create-babel-monorepo"],
+      scripts: {
+        start: "node .",
+        build: "babel -x .ts,.tsx src --out-dir dist",
+        "build:watch": "npm run build -- --watch",
+      },
+    },
+    files: ["src/index.ts"],
+  },
+  files: [
+    {
+      path: "babel.config.json",
+      contents: {
+        presets: ["@babel/preset-env"],
+        plugins: ["@babel/plugin-transform-runtime"],
+      },
+    },
+  ],
+});
